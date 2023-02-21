a543806bdeccdfa665d6a54312574bbdf629fe96
joseph <j.hernandez.92189@gmail.com>
2023-02-21 14:32:25 -0800

    added simple landing page and navbar, created client server git add -A

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+import Nav from './Components/Nav';
+import Landing from './Pages/Landing';
+
+function App() {
+  return (
+      <div>
+        <Nav></Nav>
+        <Landing></Landing>
+      </div>
+  );
+}
+
+export default App;
---
+++ client/src/Pages/Landing.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Landing = () => {
+
+
+    return (
+        <div>
+            <h1 className='text-center'>Remembering Reads</h1>
+            <div>
+                <h3 className='text-center'>Tired of struggling to remember your previous reads? We are too.</h3>
+                <p className='text-center'>
+                    Remembering Reads is a simple way to remember all of the books you have previously read.
+                    As soon as you've finished reading your current book, use our simple form to fill out a book report.
+                    Never again will someone ask "What was that book you read about" and your response being "... I remember there were aliens.. and.. government..."
+                </p>
+            </div>
+        </div>
+    );
+};
+
+export default Landing;
